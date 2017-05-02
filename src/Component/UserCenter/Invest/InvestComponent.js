598f1c7154d724a4e6f6b3650e9de3bf154ea615
shiyiwang <wangshiyiisme@gmail.com>
2017-05-02 14:42:09 +0800

    用户中心：我的投资页面更新投资列表

--- src/Component/UserCenter/Invest/InvestComponent.js
+++ src/Component/UserCenter/Invest/InvestComponent.js
@@ -5,17 +5,26 @@ import React,{Component} from 'react'
 import {
   View,
   StyleSheet,
-  Text
+  Text,
+  ScrollView,
+  Image,
+  StatusBar
 } from 'react-native'
+
+import Iconfont from 'react-native-vector-icons/Iconfont';
 
 import BackPageComponent from '../../Common/BackPageComponent'
 
 class InvestComponent extends BackPageComponent {
+  componentWillMount() {
+    StatusBar.setBarStyle('light-content')
+  }
+
   render(){
     return (
       <View style={styles.container}>
         <View style={styles.header}>
-          <Text style={styles.priceName}>投资金额（元）</Text>
+          <Text style={styles.priceName}>投资金额（元）1</Text>
           <Text style={styles.price}>47,973.00</Text>
         </View>
         <View style={styles.descBox}>
@@ -33,6 +42,38 @@ class InvestComponent extends BackPageComponent {
           <View style={styles.contentTitle}>
             <Text style={styles.contentTitleText}>投资资产明细</Text>
           </View>
+          <ScrollView style={styles.contentList}>
+            <View style={styles.contentItem}>
+              <View style={styles.imgBox}>
+                <Image
+                  source={{uri: 'http://static2.yunipo.com/images/project/covers/20170320/58cfa8b653517.jpg'}}
+                  style={{width: 108, height: 60}}
+                />
+              </View>
+              <View style={styles.infoBox}>
+                <Text style={styles.infoTitle}>【双子星-千宙】合投计划</Text>
+                <Text style={styles.infoDesc}>持有份额：10份</Text>
+              </View>
+              <View style={styles.contentIcon}>
+                <Iconfont name="right-arrow" size={18} color='#999999'></Iconfont>
+              </View>
+            </View>
+            <View style={styles.contentItem}>
+              <View style={styles.imgBox}>
+                <Image
+                  source={{uri: 'http://static2.yunipo.com/images/project/covers/20170214/58a267db13d63.jpg'}}
+                  style={{width: 108, height: 60}}
+                />
+              </View>
+              <View style={styles.infoBox}>
+                <Text style={styles.infoTitle}>云岸金服体验项目</Text>
+                <Text style={styles.infoDesc}>持有份额：3份</Text>
+              </View>
+              <View style={styles.contentIcon}>
+                <Iconfont name="right-arrow" size={18} color='#999999'></Iconfont>
+              </View>
+            </View>
+          </ScrollView>
         </View>
       </View>
     )
@@ -107,6 +148,42 @@ const styles = StyleSheet.create({
   contentTitleText: {
     fontSize: 16,
     color: '#333333'
+  },
+  contentList: {
+    paddingLeft: 15,
+  },
+  contentItem: {
+    flex: 1,
+    height: 90,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderBottomColor: '#E9E9E9'
+  },
+  imgBox: {
+    height: 100,
+    width: 120,
+    alignItems: 'flex-start',
+    justifyContent: 'center'
+  },
+  infoBox: {
+    flex: 1,
+    justifyContent: 'flex-start'
+  },
+  infoTitle: {
+    fontSize: 14,
+    color: '#333333'
+  },
+  infoDesc: {
+    paddingTop: 10,
+    fontSize: 12,
+    color: '#999999'
+  },
+  contentIcon: {
+    position: 'absolute',
+    top: 35,
+    right: 10
   }
 })
 
